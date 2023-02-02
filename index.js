bc84b3991319dc1d22f6a636c4230f108ed9c000
Jared Woelfel <jwafle@Jareds-MacBook-Pro.local>
2023-02-01 20:49:57 -0600

    update index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT;
 
 app.get('/', (req, res) => {
   res.send('Hi World!');
 });
 
-app.listen(`0.0.0.0:${env.PORT}`, () => {
-  console.log(`Example app listening at http://localhost:${env.PORT}`);
+app.listen(`0.0.0.0:${PORT}`, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
 });
