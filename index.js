0ce92c6d8ecd79057d17b1ec357e212f31235d48
Jared Woelfel <jwafle@Jareds-MacBook-Pro.local>
2023-02-01 20:57:34 -0600

    update dockerfile port

--- index.js
+++ index.js
@@ -6,6 +6,4 @@ app.get('/', (req, res) => {
   res.send('Hi World!');
 });
 
-app.listen(`0.0.0.0:${PORT}`, () => {
-  console.log(`Example app listening at http://localhost:${PORT}`);
-});
+app.listen(PORT, '0.0.0.0')
